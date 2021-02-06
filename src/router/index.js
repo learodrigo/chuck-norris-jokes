88e9a59b5aa991822860c063a998cc78853967c7
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-02-06 07:53:25 +0100

    Initial setup and 3 basic parent components

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import About from '../views/About.vue'
 import Home from '../views/Home.vue'
+import Eror404 from '../views/Eror404.vue'
 
 const routes = [
   {
@@ -10,10 +13,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/404',
+    name: 'not-found',
+    component: Eror404
   }
 ]
 
